6e8afc02710fb6adeb61a737b3b9679d218f64ec
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2022-05-11 02:09:55 +0900

    Refactor get_local_addr()
    * Rename get_addr() to get_local_addr()
    * Add more unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(test)]
+#[macro_use]
+extern crate lazy_static;
+
+#[cfg(test)]
+extern crate parking_lot;
+
 use std::env;
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
@@ -39,14 +46,14 @@ fn handle(mut stream: TcpStream) {
     }
 }
 
-fn get_addr() -> String {
+fn get_local_addr() -> String {
     let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-    let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     format!("{}:{}", host, port)
 }
 
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind(get_addr())?;
+    let listener = TcpListener::bind(get_local_addr())?;
 
     for stream in listener.incoming() {
         match stream {
@@ -60,12 +67,142 @@ fn main() -> io::Result<()> {
 }
 
 #[cfg(test)]
-mod main {
+mod test {
     use super::*;
 
+    use std::collections::HashMap;
+    use std::panic::{self, AssertUnwindSafe, UnwindSafe};
+
+    use parking_lot::Mutex;
+
+    // e.g. hashmap! { "key" => "value" }
+    #[macro_export]
+    macro_rules! hashmap(
+        { $($key:expr => $value:expr),+ } => {
+            {
+                let mut m = HashMap::new();
+                $(m.insert($key, $value);)+
+                m
+            }
+        };
+    );
+
+    fn with<T>(
+        keys: &'static str,
+        vars: HashMap<&'static str, &'static str>,
+        test: T,
+    ) where
+        T: FnOnce() + UnwindSafe,
+    {
+        lazy_static! {
+            static ref ENV_LOCK: Mutex<()> = Mutex::new(());
+        }
+        let _lock = ENV_LOCK.lock();
+
+        let result = panic::catch_unwind(AssertUnwindSafe(|| {
+            let mut origin: HashMap<&str, Result<String, env::VarError>> =
+                HashMap::new();
+
+            for key in keys.split('\n') {
+                if key.is_empty() {
+                    continue;
+                }
+
+                origin.insert(key, env::var(key));
+
+                if !vars.contains_key(key) {
+                    env::remove_var(key);
+                } else {
+                    env::set_var(key, vars.get(key).unwrap());
+                }
+            }
+
+            let result = test();
+
+            for (key, value) in origin.iter() {
+                match value {
+                    Ok(v) => env::set_var(key, v),
+                    Err(_) => env::remove_var(key),
+                }
+            }
+
+            result
+        }));
+        assert!(result.is_ok());
+    }
+
     #[test]
-    fn test_get_addr() {
-        let addr = get_addr();
-        assert_eq!("0.0.0.0:8000", addr);
+    fn test_get_local_addr_in_default() {
+        let vars: HashMap<&'static str, &'static str> = HashMap::new();
+
+        with(
+            r#"
+HOST
+PORT
+"#,
+            vars,
+            || {
+                let addr = get_local_addr();
+                assert_eq!(addr, "0.0.0.0:3000");
+            },
+        );
+    }
+
+    #[test]
+    fn test_get_local_addr_host_is_set() {
+        let vars: HashMap<&'static str, &'static str> = hashmap! {
+            "HOST" => "127.0.0.1"
+        };
+
+        with(
+            r#"
+HOST
+PORT
+"#,
+            vars,
+            || {
+                let addr = get_local_addr();
+                assert_eq!(addr, "127.0.0.1:3000");
+            },
+        );
+    }
+
+    #[test]
+    fn test_get_local_addr_port_is_set() {
+        let vars: HashMap<&'static str, &'static str> = hashmap! {
+            "PORT" => "5000"
+        };
+
+        with(
+            r#"
+HOST
+PORT
+"#,
+            vars,
+            || {
+                let addr = get_local_addr();
+                assert_eq!(addr, "0.0.0.0:5000");
+            },
+        );
+    }
+
+    #[test]
+    fn test_get_local_addr_is_set() {
+        let vars: HashMap<&'static str, &'static str> = hashmap! {
+            "HOST" => "localhost",
+            "PORT" => "5000"
+        };
+
+        with(
+            r#"
+HOST
+PORT
+"#,
+            vars,
+            || {
+                let addr = get_local_addr();
+                assert_eq!(addr, "localhost:5000");
+            },
+        );
     }
 }
